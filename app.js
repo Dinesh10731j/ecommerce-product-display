26383846f60f38a8d53f7eadea9d9a889742ad98
Dinesh10731j <dinesh1122@gmail.com>
2024-04-17 11:05:16 +0545

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const addedItems = document.querySelector(".items");
 const ShoppingCart = document.querySelector(".shopping-cart");
 const userAddeedItems = document.querySelector(".added-products");
 const productsTitle = document.querySelector(".products-title");
-
+const closeuserAddedItems = document.querySelector(".fa-xmark");
 let addedProducts = 0;
 const fetchProducts = async () => {
     try {
@@ -28,7 +28,8 @@ const fetchProducts = async () => {
             addToCartBtn.classList.add("addtocart");
             addToCartBtn.addEventListener("click", () => {
                 addedItems.textContent = ++addedProducts;
-                ShoppingItems(item.title);
+                ShoppingItems(item.title,item.thumbnail);
+                userAddeedItems.classList.add("show-added-products");
              
             });
             card.appendChild(image);
@@ -41,13 +42,20 @@ const fetchProducts = async () => {
 };
 
 
-const ShoppingItems  = (title)=>{
-    userAddeedItems.classList.add("show-added-products");
+const ShoppingItems  = (title,productsimage)=>{
 const createTitle = document.createElement("h6");
+const createImg = document.createElement("img");
+createImg.src = productsimage;
+createImg.classList.add("added-products-info-img")
 createTitle.innerText = title;
 userAddeedItems.appendChild(createTitle);
+userAddeedItems.appendChild(createImg)
 }
 
+
+closeuserAddedItems.addEventListener("click",()=>{
+    userAddeedItems.classList.remove("show-added-products"); 
+})
 
 ShoppingCart.addEventListener("click",ShoppingItems);
 fetchProducts();
