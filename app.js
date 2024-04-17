1b6d3362ae9115567448b9249a3c25b476ebfd12
Dinesh10731j <dinesh1122@gmail.com>
2024-04-17 09:59:05 +0545

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const productContainer = document.querySelector(".Products");
 const addedItems = document.querySelector(".items");
+const ShoppingCart = document.querySelector(".shopping-cart");
 let addedProducts = 0;
 const fetchProducts = async () => {
     try {
@@ -15,7 +16,6 @@ const fetchProducts = async () => {
         items.forEach(item => {
             const card = document.createElement("div");
             card.classList.add("card");
-
             const image = document.createElement("img");
             image.classList.add("productsimages");
             image.src = item.thumbnail;
@@ -25,6 +25,7 @@ const fetchProducts = async () => {
             addToCartBtn.classList.add("addtocart");
             addToCartBtn.addEventListener("click", () => {
                 addedItems.textContent = ++addedProducts;
+                alert(item.title)
             });
 
             card.appendChild(image);
@@ -35,6 +36,14 @@ const fetchProducts = async () => {
         console.log("Error fetching products:", error);
     }
 };
+
+
+const ShoppingItems  = ()=>{
+    alert("Hello world")
+}
+
+
+ShoppingCart.addEventListener("click",ShoppingItems)
 
 
 //initial function call 
